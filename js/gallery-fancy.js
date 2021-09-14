d025f33886da97ad811448e27c7e2d95afa73aa1
gaponova <dzensmart@gmail.com>
2021-09-14 16:56:15 +0300

    Сделали адаптив для блока program-header и зацикленность слайдов в галерее блока work

---
+++ js/gallery-fancy.js
@@ -0,0 +1,13 @@
+$("[data-fancybox]").fancybox( {
+  loop: true,
+  transitionDuration: 2000,
+  buttons: [
+    // "zoom",
+    //"share",
+    // "slideShow",
+    //"fullScreen",
+    //"download",
+    "thumbs",
+    "close"
+  ],
+});
