29085a184b862a805156160803fdaeebdcdd0ab3
gaponova <dzensmart@gmail.com>
2021-09-19 19:48:56 +0300

    Добавили в разметку слайдер

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    margin: 10,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    dots: true,
+    center: true
+  });
+  
+});
